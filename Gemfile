f48f564dc34dabc76161d0429d1b0ed47982630c
NavPoint1 <foleym156@gmail.com>
2020-10-01 15:01:45 -0700

    f i n i s h e d

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.6.1'
 
 gem 'semantic-ui-rails'
 gem 'semantic-ui-sass', github: 'doabit/semantic-ui-sass'
+gem 'animate.css-rails', '~> 3.2.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.3'
--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,18 +1,30 @@
 class SpeciesController < ApplicationController
 
+    START_ANIMATIONS = ["rollIn", "bounceInLeft", "rotateInDownRight", "fadeInUpBig", "zoomInUp", "zoomIn", "rotateInUpRight"]
+    BREAK_ANIMATIONS = ["wobble", "rubberBand"]
+    CATCH_ANIMATIONS = ["zoomOut"]
+    FLEE_ANIMATIONS = ["lightSpeedOut", "fadeOutUpBig", "rollOut"]
+
     def show
         @species = Species.find(params[:id])
         @flag = 1
+        @animation = START_ANIMATIONS.sample
     end
     
     def catch
         @species = Species.find(params[:id])
         if @species.catch_trigger
-            @flag = 2        
+            # caught it
+            @flag = 2     
+            @animation = CATCH_ANIMATIONS.sample 
         elsif @species.flee_trigger
+            # got away
             @flag = 3
+            @animation = FLEE_ANIMATIONS.sample
         else
+            # broke free
             @flag = 4
+            @animation = BREAK_ANIMATIONS.sample
         end
         render :show
     end
